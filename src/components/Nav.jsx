03f3679640af0bb538264154e9bf259c59d0f9c8
Vinicius <viniciusbittencourt113@gmail.com>
2024-09-04 02:24:18 -0300

    Criando e estilizando a navbar do site

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,31 @@
 import { Link } from "react-router-dom"
 import { NavStyle } from "../css/NavStyle"
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Nav = () => {
     return(
         <>
+            
             <NavStyle>
-                <section className="nav">
-                    <Link to='/' className="link">Home</Link>
-                    <Link to='/produtos' className="link">Produtos</Link>
-                    <Link to='/sobre' className="link">Sobre</Link>
-                    <Link to='/contato' className="link">Contato</Link>
+                <section id="nav">
+                    <h1>Foto Site</h1>
+                    <nav className="menuNav">
+                        <ul className="menuLink">
+                            <li><Link to='/' className="link">Home</Link></li>
+                            <li><Link to='/produtos' className="link">Produtos</Link></li>
+                            <li><Link to='/sobre' className="link">Sobre</Link></li>
+                            <li><Link to='/contato' className="link">Contato</Link></li>
+                        </ul>
+                        <ul className="menuLogin">
+                            <li>Login</li>
+                            <li>Cadastro</li>
+                        </ul>
+                        <ul className="menuBag">
+                            <i class="bi bi-bag"></i>   
+                        </ul>
+
+                    </nav>
+
                 </section>
             </NavStyle>
         </>
--- src/css/NavStyle.jsx
+++ src/css/NavStyle.jsx
@@ -2,23 +2,77 @@ import styled from 'styled-components'
 
 export const NavStyle = styled.section `
 
-.nav{
-    width: 100%;
+*{
+    font-family: "Work Sans", sans-serif;
+}
+
+#nav{
+    display: flex;
+    background-color: #EEEEF0;
+    padding: 0 6%;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.menuNav{
+    width: 50%;
     height: 70px;
-    list-style: none;
     gap: 1rem;
-    background-image: linear-gradient(to left, aqua, blue);
     display: flex;
     align-items: center;
+    justify-content: space-between;
+}
+
+.menuLink{
+    list-style: none;
+    display: flex;
+    gap: 45px;
 }
 
 .link{
-    font-size: 1rem;
-    color: #fff;
-    font-weight: 700;
-    padding: 20px;
-    text-transform: uppercase;
+    font-size: 1.08rem;
+    color: black;
+    font-weight: 500;
+    text-transform: capitalize;
     text-decoration: none;
+    position: relative;
+    padding-bottom: 4px;
+}
+
+.link::after{
+    content: " ";
+    width: 0;
+    height: 2px;
+    background-color: black;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    transition: 0.3s ease-in-out;
+}
+
+.link:hover::after{
+    width: 100%;
+}
+
+.menuNav i{
+    color: black;
+    font-size: 18px;
+} 
+
+.menuBag{
+    background-color: #D3DE62;
+    padding: 10px;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.menuLogin{
+    list-style: none;
+    text-align: end;
+    font-size: 12px;
+    cursor: pointer;
+    font-weight: 600;
+    margin-right: -30px;
 }
 
 `
