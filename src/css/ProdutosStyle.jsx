76739b33c0c0670520873f3da2fdbc310e2b1d06
Lucas <lucasludovicocabraal@gmail.com>
2024-09-06 16:27:01 -0300

    Add product page

--- src/css/ProdutosStyle.jsx
+++ src/css/ProdutosStyle.jsx
@@ -2,4 +2,50 @@ import styled from 'styled-components'
 
 export const ProdutosStyle = styled.section `
 
+
+.product-container {
+    padding-top: 150px;
+    padding-bottom: 200px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr); /* Cria 4 colunas de igual largura */
+    gap: 10px; /* Espaço entre os blocos */
+    align-items: center; /* Alinha as imagens verticalmente */
+    justify-items: center; /* Alinha as imagens horizontalmente */
+}
+
+.product-card{
+    cursor: pointer;
+}
+
+.product-img{
+    background-color: #f8f8f8;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px #f0f0f0;
+}
+
+.product-img img {
+    width: 350px;
+    margin-bottom: -1%;
+}
+
+.product-info-and-btn-container{
+    display: flex;
+}
+
+.tittle-price-container{
+    padding: 10%;
+}
+.tittle-price-container p{
+    font-size: 20px;
+    padding-bottom: 10%;
+}
+
+.add-btn{
+    padding: 4%;
+    padding-top: 8%;
+    margin-left: 30%;
+    color: #D3DE62;
+    font-size: 30px;
+}
+
 `
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,80 @@
 import { ProdutosStyle } from "../css/ProdutosStyle"
+import  Product1  from '../assets/products/product-1.png'
+import  Product2  from '../assets/products/product-2.png'
+import  Product3  from '../assets/products/product-3.png'
+import  Product4  from '../assets/products/product-4.png'
+
+
 
 const Produtos = () => {
     return(
-        <>
-            <h1>Produtos</h1>
-        </>
+        <ProdutosStyle>
+            <section>
+                <div class="product-container">
+
+                    <div class="product-card">
+                        <div class="product-img">
+                            <img src={Product1} alt="Product 1"/>
+                        </div>
+                        <div className="product-info-and-btn-container">
+                            <div className="tittle-price-container">
+                                <p>Tank Top</p>
+                                <h3>$40</h3>
+                            </div>
+                            <div className="add-btn">
+                                <i class="bi bi-plus-square-fill"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="product-card">
+                        <div class="product-img">
+                            <img src={Product2} alt="Product 1"/>
+                        </div>
+                        <div className="product-info-and-btn-container">
+                            <div className="tittle-price-container">
+                                <p>Tracksuit</p>
+                                <h3>$69</h3>
+                            </div>
+                            <div className="add-btn">
+                                <i class="bi bi-plus-square-fill"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="product-card">
+                        <div class="product-img">
+                            <img src={Product3} alt="Product 1"/>
+                        </div>
+                        <div className="product-info-and-btn-container">
+                            <div className="tittle-price-container">
+                                <p>Sweatsuit</p>
+                                <h3>$79</h3>
+                            </div>
+                            <div className="add-btn">
+                                <i class="bi bi-plus-square-fill"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="product-card">
+                        <div class="product-img">
+                            <img src={Product4} alt="Product 1"/>
+                        </div>
+                        <div className="product-info-and-btn-container">
+                            <div className="tittle-price-container">
+                                <p>Black Outfit</p>
+                                <h3>$50</h3>
+                            </div>
+                            <div className="add-btn">
+                                <i class="bi bi-plus-square-fill"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </ProdutosStyle>
     )
 }
 
