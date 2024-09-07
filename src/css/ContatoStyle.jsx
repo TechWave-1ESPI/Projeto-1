ec6aafb47cf66ca25d6322c27e02e715e1458a49
Weslley <weslleycardoso011@gmail.com>
2024-09-07 00:49:51 -0300

    Adicionando Comntato e estilizando

--- src/css/ContatoStyle.jsx
+++ src/css/ContatoStyle.jsx
@@ -1,5 +1,48 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const ContatoStyle = styled.section `
+export const ContatoStyle = styled.section`
+  background-color: #D9E14F;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+  max-width: 600px;
+  margin: 50px auto;
+  text-align: center;
+`;
 
-`
+export const FormStyle = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const InputStyle = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 2px solid #D3DE62;
+  border-radius: 5px;
+  font-size: 1rem;
+  background-color: white;
+
+  &:focus {
+    outline: none;
+    border-color: #b0b38f9e;
+    box-shadow: 0px 0px 10px #b0b38f9e;
+  }
+`;
+
+export const ButtonStyle = styled.button`
+  padding: 12px 20px;
+  background-color: #D3DE62;
+  border: none;
+  border-radius: 5px;
+  font-weight: 500;
+  transition: 0.3s;
+  box-shadow: 0px 0px 10px #b0b38f9e;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #b0b38f;
+    box-shadow: 0px 0px 20px #b0b38f;
+  }
+`;
--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,11 +1,23 @@
-import { ContatoStyle } from "../css/ContatoStyle"
+import { ContatoStyle, FormStyle, InputStyle, ButtonStyle } from "../css/ContatoStyle"
 
 const Contato = () => {
-    return(
-        <>
-            <h1>Contato</h1>
-        </>
+    return (
+        <ContatoStyle>
+            <h1>Contact Us</h1>
+            <FormStyle>
+                <label htmlFor="email">Your email:</label>
+                <InputStyle type="email" id="email" name="email" placeholder="Enter your email" required />
+
+                <label htmlFor="phone">Your phone number:</label>
+                <InputStyle type="tel" id="phone" name="phone" placeholder="Enter your phone number" required />
+
+                <label htmlFor="message">Your message:</label>
+                <InputStyle as="textarea" id="message" name="message" rows="4" placeholder="Write your message" required />
+
+                <ButtonStyle type="submit">Send</ButtonStyle>
+            </FormStyle>
+        </ContatoStyle>
     )
 }
 
-export default Contato
+export default Contato;
